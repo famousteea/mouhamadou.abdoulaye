0811a4c9f83675487e6879ddd7609d6c673b9b4d
moustapha <mbmoustapha4005@gmail.com>
2023-11-15 15:59:20 -0500

    init

---
+++ adresse.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <form method="post" action="adresse.php">
+  <label for="addressCount">Combien d'adresses avez-vous ?</label>
+  <input type="number" name="addressCount" id="addressCount" min="1" required>
+  <button type="submit">Suivant</button>
+</form>
+</div>
